7bb11f97164b82f5c64990af49d2550a887deffe
Ahmet <2110206036@ogrenci.karabuk.edu.tr>
2025-07-15 17:19:25 +0300

    initial commit

---
+++ basic-security-app/src/main/java/com/ahmetsezer/basicsecurityapp/BasicSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.ahmetsezer.basicsecurityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSecurityAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSecurityAppApplication.class, args);
+    }
+
+}
---
+++ basic-security-app/src/test/java/com/ahmetsezer/basicsecurityapp/BasicSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmetsezer.basicsecurityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSecurityAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
